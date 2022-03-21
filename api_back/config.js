f4fa64cebceaf87f8b6f8ca65456fbc46b3929f5
webtoufik <webtoufik@gmail.com>
2022-03-21 10:41:10 +0100

    first commit

---
+++ api_back/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	db: {
+		host: "eu-cdbr-west-02.cleardb.net",
+		database: "heroku_20923968da5c994",
+		user: "b80d785a972f28",
+		password: "20e98c45"
+		//port: 8889
+	},
+	token: {
+		secret: "muaykhao"
+	}
+}
---
+++ api_back/withAuth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const secret = "muaykhao";
+
+
+const withAuth = (req, res, next)=>{
+    const token = req.headers['x-access-token'];
+    console.log('Token dans withAuth',token);
+    
+    jwt.verify(token, secret, (err, decode)=>{
+        console.log(decode);
+        
+        if(err) {
+          console.log(err);
+          res.json({status: 401, err: err})
+        } else {
+            req.id = decode.id;
+            req.email = decode.email;
+            next();
+        }
+    })
+    
+    
+}
+
+module.exports = withAuth;
